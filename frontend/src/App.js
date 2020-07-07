9323f4e8607f0126e12ef4dbed364ad42bfb85e8
p-cap <64616459+p-cap@users.noreply.github.com>
2020-07-07 06:33:51 -0700

    Still trying to graps the git concept but renamed my directory and removed some of the files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyForm from './MyForm'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          SSO
-        </p>
+        <h1>
+          Test Scheduler 
+        </h1>
+        <MyForm />
         <a
           className="App-link"
           href="https://reactjs.org"
